9f58d03df72496ae528d6dca462825cc8ddaed18
Forgotten-shadow <villagerguard@gmail.com>
2026-04-22 19:37:40 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,6 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  assetsInclude: ['**/*.JPG'],
   base: './', // Важно: точка перед слэшем помогает путям не ломаться
 })
